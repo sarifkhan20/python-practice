1cb8e2d17d54ea270a5386ddf2b29de1b570400f
Sarif Khan <sarifkh1213@gmail.com>
2026-08-14 00:08:06 +0530

    updated py.9

--- 09_lists.py
+++ 09_lists.py
@@ -5,11 +5,13 @@ sarif=["sarif",700,"rupye",6.7,False]
 
 sarif.append("khan")
 #print(sarif)
-l1=[1,40,734,23,9865,1,56]
-#l1.sort()
-#l1.reverse()
-#l1.insert(3,4)
+# l1=[1,40,734,23,9865,1,56]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3,4)
 # print(l1.pop(3))
 # l1.pop(4)
-l1.remove(734)
-print(l1)
+
+
+# l1.remove(734)
+# print(l1.pop(4))
