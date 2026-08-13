a291770c927260ddc4aa1f2a2dd8fe9699d58e4e
Sarif Khan <sarifkh1213@gmail.com>
2026-08-14 00:37:36 +0530

    added some more programs

---
+++ 10_tuples.py
@@ -0,0 +1,37 @@
+a=(1,45,3.5,45,45,"sarif")
+#tuples cant be chnaged
+
+
+# print(type(a))
+# print(a)
+
+
+# no=a.count(45)
+# print(no)
+
+
+# i=a.index("sarif")
+# print(i)
+
+
+#print(45 in a)
+
+
+# new_tuple=a*2
+# print(new_tuple)
+
+
+# union=a+new_tuple
+# print(union)
+
+
+# print(len(a))
+
+
+#print(min(a)) #change the string into the int first(dont work in int+str)
+
+
+#print(max(a))
+
+b,c,d,e,f,g=a
+print(b,c,d,e,f,g)
---
+++ 11_practice_set.py
@@ -0,0 +1,36 @@
+# fruits = []
+# f1=input("enter fruit name")
+# fruits.append(f1)
+# f1=input("enter fruit name")
+# fruits.append(f1)
+# f1=input("enter fruit name")
+# fruits.append(f1)
+# f1=input("enter fruit name")
+# fruits.append(f1)
+# f1=input("enter fruit name")
+# fruits.append(f1)
+# print(fruits)
+
+
+# marks = []
+# f1=int(input("enter fruit name:"))
+# marks.append(f1)
+# f1=int(input("enter fruit name:"))
+# marks.append(f1)
+# f1=int(input("enter fruit name:"))
+# marks.append(f1)
+# f1=int(input("enter fruit name:"))
+# marks.append(f1)
+# f1=int(input("enter fruit name:"))
+# marks.append(f1)
+# marks.sort()
+# print(marks)
+
+
+# l=[34,78,12]
+# print(sum(l))
+
+
+# a=(7,0,8,0,0,9)
+# n=a.count(0)
+# print(n)
