7f506fcb3d5f54cf720c240b32aebdd0db942387
Sarif Khan <sarifkh1213@gmail.com>
2026-08-17 22:36:02 +0530

    added dictionary

---
+++ 12_dictionary.py
@@ -0,0 +1,28 @@
+marks={
+    "sarif": 100,
+    "khan": 34,
+    "beswa": 65,
+    0: "don"
+}
+#print(marks,type(marks))
+# marks["sarif"]=marks["sarif"]+1
+#print(marks[0])
+
+
+# print(marks.items())
+
+
+# print(marks.keys())
+
+
+# print(marks.values())
+
+
+# marks.update({"sarif": 99,"renuka": 100})
+# print(marks)
+
+
+print(marks.get("sarif2"))
+print(marks["sarif"])
+
+# seaech on gpt for methods of dict.
